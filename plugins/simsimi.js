08762ec94315f04071c84267d4541366d59f43ce
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-03-24 19:13:23 +0700

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,13 +1,13 @@
 let fetch = require('node-fetch')
-
 let handler = async (m, { text, usedPrefix, command }) => {
-  if (!text) throw `Penggunaan:\n${usedPrefix + command} <teks>\n\nContoh:\n${usedPrefix + command} hai`
-  let res = await fetch(API('https://api.simsimi.net', '/v2/', { text, lc: id }))
+  if (!text) throw `uhm.. teksnya mana?\n\ncontoh:\n${usedPrefix + command} hai`
+  let res = await fetch(global.API('pencarikode', '/api/simsimii', { text: encodeURIComponent(text) }, 'apikey'))
   if (!res.ok) throw eror
   let json = await res.json()
-  await m.reply(`*Simi:* ${json.success}`)
+  if (json.result == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.') await m.reply('siminya blom diajarin jadi gatau t_t custom pesannya di https://simsimi.com/teach')
+  await m.reply(`*Simi:* ${json.result}`)
 }
-handler.help = ['simi', 'simsimi'].map(v => v + ' <teks>')
+handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
 handler.tags = ['fun']
 handler.command = /^((sim)?simi|simih)$/i
 
