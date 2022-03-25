94f01232fb07ac00e235f4d7ddbdf976058556ae
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-03-25 15:53:28 +0700

    Update tebakbendera_hint.js

--- plugins/tebakbendera_hint.js
+++ plugins/tebakbendera_hint.js
@@ -1,24 +1,10 @@
-const similarity = require('similarity')
-const threshold = 0.72
-let handler = m => m
-handler.before = async function (m) {
+let handler = async (m, { conn }) => {
+    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*benderaapa/i.test(m.quoted.text)) return !0
-    this.tebakbendera = this.tebakbendera ? this.tebakbendera : {}
-    if (!(id in this.tebakbendera)) return m.reply('Soal itu telah berakhir')
-    if (m.quoted.id == this.tebakbendera[id][0].id) {
-        let json = JSON.parse(JSON.stringify(this.tebakbendera[id][1]))
-        // m.reply(JSON.stringify(json, null, '\t'))
-        if (m.text.toLowerCase() == json.name.toLowerCase().trim()) {
-            global.DATABASE._data.users[m.sender].exp += this.tebakbendera[id][2]
-            m.reply(`*Benar!*\n+${this.tebakbendera[id][2]} XP`)
-            clearTimeout(this.tebakbendera[id][3])
-            delete this.tebakbendera[id]
-        } else if (similarity(m.text.toLowerCase(), json.name.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
-        else m.reply(`*Salah!*`)
-    }
-    return !0
+    if (!(id in conn.tebakbendera)) throw false
+    let json = conn.tebakbendera[id][1]
+    m.reply('```' + json.name.replace(/[bcdfghjklmnpqrstvwxyz]/g, '_') + '```')
 }
-handler.exp = 0
-
+handler.command = /^benderaapa$/i
+handler.limit = true
 module.exports = handler
