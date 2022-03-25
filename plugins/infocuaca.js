51055c2d81817b67857d727e07fccdf4c9afd6b9
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-03-25 11:06:11 +0700

    Create infocuaca.js

---
+++ plugins/infocuaca.js
@@ -0,0 +1,24 @@
+let fetch = require('node-fetch')
+let handler = async(m, { conn, text }) => {
+await m.reply(global.wait)
+    let res = await (await fetch(`https://zahirr-web.herokuapp.com/api/infocuaca`)).json()
+        let str = `*INFO CUACA*\n\nNama kota : ${res.nama_kota}\nKelembapan : ${res.Kelembapan}\nSuhu : ${res.suhu}`
+    conn.sendButtonLoc(m.chat, await (await fetch(res.map)).buffer(), str, footer, 'News', '#? news', m)
+}
+handler.help = ['infocuaca']
+handler.tags = ['news']
+handler.command = /^(infocuaca)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+handler.limit = false
+
+module.exports = handler
