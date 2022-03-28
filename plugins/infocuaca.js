d01c1cd8e5f83efbb13e91d77a3c34ede570d4b3
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-03-29 06:25:11 +0700

    Update infocuaca.js

--- plugins/infocuaca.js
+++ plugins/infocuaca.js
@@ -1,9 +1,9 @@
 let fetch = require('node-fetch')
 let handler = async(m, { conn, text }) => {
 await m.reply(global.wait)
-    let res = await (await fetch(`https://zahirr-web.herokuapp.com/api/infocuaca`)).json()
-        let str = `*INFO CUACA*\n\nTittle : ${res.tittle}\nKelembapan : ${res.kelembapan}\nSuhu : ${res.suhu}`
-    conn.sendButtonLoc(m.chat, await (await fetch(res.map)).buffer(), str, footer, 'News', '#? news', m)
+    let res = await (await fetch(`https://api.lolhuman.xyz/api/cuaca`)).json()
+        let str = `*INFO CUACA*\n\nTempat : ${res.tempat}\nLatitude : ${res.latitude}\nLongitude : ${res.longitude}\nCuaca : ${res.cuaca}\nAngin : ${res.angin}\nDeskripsi : ${res.description}\nKelembapan : ${res.kelembapan}\nSuhu : ${res.suhu}\nUdara : ${res.udara}\nPermukaan Laut : ${res.permukaan_laut}`
+    conn.sendButtonLoc(m.chat, 'infocuaca.jpg', str, footer, 'News', '#? news', m)
 }
 handler.help = ['infocuaca']
 handler.tags = ['news']
