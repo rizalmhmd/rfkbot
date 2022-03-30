175d3f62cabfe4f48e0ca1bc50a9d7dddd5b1df1
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-03-30 15:35:07 +0700

    Update ttporn.js

--- plugins/ttporn.js
+++ plugins/ttporn.js
@@ -2,10 +2,10 @@ let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
 conn.sendFile(m.chat, 'https://zenzapi.xyz/downloader/tikporn?apikey=sanzychan01', 'asupan.mp4', 'Nih Jngn comly  ya', m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
 }
-handler.help = ['bkp', 'bokep']
+handler.help = ['ttporn']
 handler.tags = ['bokep']
 
-handler.command = /^(bkp|bokep)$/i
+handler.command = /^(ttporn)$/i
 handler.premium = false
 handler.register = true
 handler.limit = true
