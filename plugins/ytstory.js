8711315528afb869dad3282dd3f5aee11270d79f
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-04-12 22:14:12 +0700

    Update ytstory.js

--- plugins/ytstory.js
+++ plugins/ytstory.js
@@ -2,12 +2,12 @@ let fetch = require('node-fetch')
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
     if (!text) throw `uhm.. urlnya mana?\n\npenggunaan:\n${usedPrefix + command} url\ncontoh:\n${usedPrefix + command} https://youtube.com/shorts/lBQb1rvY9ao`
-    let res = await fetch(`https://api.lolhuman.xyz/api/tiktok?apikey=${lolkey}&url=${args[0]}`)
+    let res = await fetch(global.API('lol', '/api/ytreels', 'apikey', { url: text }))
     if (!res.ok) throw eror
     let json = await res.json()
     if (!json.status) throw json
     await m.reply(wait)
-    await conn.sendFile(m.chat, json.result.link, json.result.title, wm, m)
+    await conn.sendFile(m.chat, json.result.link, json.result.title, m)
 }
 handler.help = ['ytstory'].map(v => v + ' <url>')
 handler.tags = ['downloader']
