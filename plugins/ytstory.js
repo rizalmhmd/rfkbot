4dadcf7a89eacfbe54dcb0305bf8dfdeff4961de
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-04-12 20:58:44 +0700

    Update ytstory.js

--- plugins/ytstory.js
+++ plugins/ytstory.js
@@ -2,12 +2,12 @@ let fetch = require('node-fetch')
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
     if (!text) throw `uhm.. urlnya mana?\n\npenggunaan:\n${usedPrefix + command} url\ncontoh:\n${usedPrefix + command} https://youtube.com/shorts/lBQb1rvY9ao`
-    let res = await fetch(API('LeysCoder', '/api/ytdl-story', { url: text }, 'apikey'))
+    let res = await fetch(API('lol', '/api/ytreels', apikey, {url : text},'))
     if (!res.ok) throw eror
     let json = await res.json()
     if (!json.status) throw json
     await m.reply(wait)
-    await conn.sendFile(m.chat, json.result.link, json.result.nama, wm, m)
+    await conn.sendFile(m.chat, json.result.link, json.result.title, wm, m)
 }
 handler.help = ['ytstory'].map(v => v + ' <url>')
 handler.tags = ['downloader']
