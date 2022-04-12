9aaad889859c6ff186450d59d1ac88049cb28979
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-04-12 14:44:10 +0700

    Update family100_answer.js

--- plugins/family100_answer.js
+++ plugins/family100_answer.js
@@ -1,5 +1,6 @@
 const similarity = require('similarity')
-const threshold = 0.72 // semakin tinggi nilai, semakin mirip
+const threshold = 0.72
+
 module.exports = {
     async before(m) {
         this.game = this.game ? this.game : {}
@@ -9,37 +10,33 @@ module.exports = {
         let text = m.text.toLowerCase().replace(/[^\w\s\-]+/, '')
         let isSurrender = /^((me)?nyerah|surr?ender)$/i.test(m.text)
         if (!isSurrender) {
-            let index = room.jawaban.indexOf(text)
+            let index = room.jawaban.findIndex(v => v.toLowerCase().replace(/[^\w\s\-]+/, '') === text)
             if (index < 0) {
-                if (Math.max(...room.jawaban.filter((_, index) => !room.terjawab[index]).map(jawaban => similarity(jawaban, text))) >= threshold) m.reply('Dikit lagi!')
+                if (Math.max(...room.jawaban.filter((_, index) => !room.terjawab[index]).map(jawaban => similarity(jawaban, text))) >= threshold) m.reply(dikit)
                 return !0
             }
             if (room.terjawab[index]) return !0
-            let users = global.DATABASE.data.users[m.sender]
+            let users = global.db.data.users[m.sender]
             room.terjawab[index] = m.sender
             users.exp += room.winScore
         }
         let isWin = room.terjawab.length === room.terjawab.filter(v => v).length
         let caption = `
-*Soal:* ${room.soal}
+*soal:* ${room.soal}
 
-Terdapat *${room.jawaban.length}* jawaban${room.jawaban.find(v => v.includes(' ')) ? `
+terdapat *${room.jawaban.length}* jawaban${room.jawaban.find(v => v.includes(' ')) ? `
 (beberapa jawaban terdapat spasi)
 `: ''}
-${isWin ? `*SEMUA JAWABAN TERJAWAB*`: isSurrender ? '*MENYERAH!*' : ''}
+${isWin ? `*SEMUA JAWABAN TERJAWAB*` : isSurrender ? '*MENYERAH!*' : ''}
 ${Array.from(room.jawaban, (jawaban, index) => {
-            return isSurrender || room.terjawab[index] ? `(${index + 1}) ${jawaban} ${room.terjawab[index] ? '@' + room.terjawab[index].split('@')[0] : ''}`.trim() : false
+            return isSurrender || room.terjawab[index] ? `(${index + 1}) ${jawaban} ${room.terjawab[index] ? '@' + room.terjawab[index].split`@`[0] : ''}`.trim() : false
         }).filter(v => v).join('\n')}
 
-${isSurrender ? '' : `+${room.winScore} XP tiap jawaban benar`}
+${isSurrender ? '' : ``}
     `.trim()
-        m.reply(caption, null, {
-            contextInfo: {
-                mentionedJid: this.parseMention(caption)
-            }
-        }).then(msg => {
+        await this.sendButton(m.chat, caption, wm, `${isWin || isSurrender ? 'lagi' : 'nyerah'}`, `${isWin || isSurrender ? '.family100' : 'nyerah'}`, m).then(msg => {
             return this.game[id].msg = msg
-        })
+        }).catch(_ => _)
         if (isWin || isSurrender) delete this.game[id]
         return !0
     }
