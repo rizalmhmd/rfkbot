e7e3144fbe591b4a2a79e7fb4bf25788d5a4c88d
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-04-12 14:56:40 +0700

    Update and rename link.js to linkgroup.js

--- plugins/linkgroup.js
+++ plugins/linkgroup.js
@@ -16,11 +16,11 @@ handler.command = /^link(gro?up)?$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
-handler.group = false
+handler.group = true
 handler.private = false
 
-handler.admin = false
-handler.botAdmin = false
+handler.admin = true
+handler.botAdmin = true
 
 handler.fail = null
 
