019c996c67f30b4f95a078c91790be352eea4083
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-04-12 15:10:38 +0700

    Update darkjoke.js

--- plugins/darkjoke.js
+++ plugins/darkjoke.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('xteam', '/asupan/darkjoke', {}, 'APIKEY'), 'Dark Jokes', 'NEXT', '.darkjokes', m)
+    conn.sendFile(m.chat, global.API('xteam', '/asupan/darkjoke', {}, 'APIKEY'), '', 'drag joles', m)
 }
 handler.help = ['darkjokes']
 handler.tags = ['internet']
