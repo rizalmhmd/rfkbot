d79fbe88fe1bf96245ab0f3143b21f2c477ad53a
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-04-13 11:17:18 +0700

    Update tekateki.js

--- plugins/tekateki.js
+++ plugins/tekateki.js
@@ -1,17 +1,18 @@
 let fetch = require('node-fetch')
 let timeout = 120000
 let poin = 500
+
 let handler = async (m, { conn, usedPrefix }) => {
     conn.tekateki = conn.tekateki ? conn.tekateki : {}
     let id = m.chat
-    if (id in conn.tekateki) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tekateki[id][0])
-        throw false
-    }
-    let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tekateki.json')).json()
-    let json = src[Math.floor(Math.random() * src.length)]
+    if (id in conn.tekateki) return conn.reply(m.chat, 'Belum dijawab!', conn.tekateki[id][0])
+    let res = await fetch(API('amel', '/tekateki', {}, 'apikey'))
+    if (!res.ok) throw eror
+    let json = await res.json()
+    if (!json.status) throw json
     let caption = `
 ${json.soal}
+
 Timeout *${(timeout / 1000).toFixed(2)} detik*
 Ketik ${usedPrefix}tete untuk bantuan
 Bonus: ${poin} XP
@@ -19,8 +20,8 @@ Bonus: ${poin} XP
     conn.tekateki[id] = [
         await conn.sendButton(m.chat, caption, 'ROZZxBOTZ', 'Bantuan', `.tete`, m),
         json, poin,
-        setTimeout(async () => {
-            if (conn.tekateki[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, 'ROZZxBOTZ', 'Teka Teki', `.tekateki`, conn.tekateki[id][0])
+        setTimeout(() => {
+            if (conn.tekateki[id]) conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, 'ROZZxBOTZ', 'Teka Teki', `.tekateki`, conn.tekateki[id][0])
             delete conn.tekateki[id]
         }, timeout)
     ]
@@ -28,5 +29,7 @@ Bonus: ${poin} XP
 handler.help = ['tekateki']
 handler.tags = ['game']
 handler.command = /^tekateki/i
+
+handler.game = true
 
 module.exports = handler
