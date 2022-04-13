10247fc076a9049d1cd37846c7dfa379c789d42b
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-04-13 13:43:20 +0700

    Update tod.js

--- plugins/tod.js
+++ plugins/tod.js
@@ -1,9 +1,6 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, command, usedPrefix }) => {
-  if (/^tod$/i.test(command)) {
-    await conn.send3Button(m.chat, 'Truth or Dare', 'ROZZxBOTZ', 'Truth', `${usedPrefix}truth`, 'Dare', `${usedPrefix}dare`, 'Random', `${conn.pickRandom([`${usedPrefix}dare`, `${usedPrefix}truth`])}`, m)
-  }
   if (/^truth$/i.test(command)) {
     let res = await fetch(API('amel', '/truth', {}, 'apikey'))
     if (!res.ok) throw eror
