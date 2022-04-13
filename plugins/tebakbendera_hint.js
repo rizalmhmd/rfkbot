e1de2f8913f95b8a44d09ff7683b49e1e67949f6
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-04-13 14:27:03 +0700

    Update tebakbendera_hint.js

--- plugins/tebakbendera_hint.js
+++ plugins/tebakbendera_hint.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn }) => {
-    conn.tebakgambar = conn.tebakbendera ? conn.tebakbendera : {}
+    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
     let id = m.chat
     if (!(id in conn.tebakbendera)) throw false
     let json = conn.tebakbendera[id][1]
-    conn.reply(m.chat, '```' + json.jawaban.replace(/[AIUEOaiueo]/g, '_') + '```\nBalas gambarnya, bukan pesan ini', conn.tebakbendera[id][0])
+    m.reply('```' + json.name.replace(/[bcdfghjklmnpqrstvwxyz]/g, '_') + '```')
 }
 handler.command = /^benderaapa$/i
-
+handler.limit = true
 module.exports = handler
