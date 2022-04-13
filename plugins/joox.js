530063616be1c1ea5b896b4862be6d06b826b4be
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-04-13 11:29:53 +0700

    Update joox.js

--- plugins/joox.js
+++ plugins/joox.js
@@ -1,19 +1,26 @@
 const fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `uhm.. judul nya apa?\n\ncontoh:\n${usedPrefix + command} akad`
+    if (!text) throw `Pengunaan:\n${usedPrefix + command} <teks>\n\nContoh:\n${usedPrefix + command} akad`
     if (isUrl(text)) throw `uhm.. judul kak bukan pake url\n\ncontoh:\n${usedPrefix + command} akad`
 
-                let anu = await fetch(global.API('zenz', '/downloader/joox', { query: text }, 'apikey'))
-                let msg = await hisoka.sendImage(m.chat, anu.result.img, `⭔ Title : ${anu.result.lagu}\n⭔ Album : ${anu.result.album}\n⭔ Singer : ${anu.result.penyanyi}\n⭔ Publish : ${anu.result.publish}\n⭔ Lirik :\n${anu.result.lirik.result}`, m)
-                hisoka.sendMessage(m.chat, { audio: { url: anu.result.mp4aLink }, mimetype: 'audio/mpeg', fileName: anu.result.lagu+'.m4a' }, { quoted: msg })
+    let res = await fetch(API('zenz', '/downloader/joox', { query: text }, 'apikey'))
+    if (!res.ok) throw await `${res.status} ${res.statusText}`
+    let json = await res.json()
+    if (!json.status) throw json
+    let { judul, artist, album, img_url, mp3_url, filesize, duration } = json.result
+    let pesan = `
+Judul: ${judul}
+Artis: ${artist}
+Album: ${album}
+Ukuran File: ${filesize}
+Durasi: ${duration}
 
-© ROZZxBOTZ
+ROZZxBOTZ
     `.trim()
 
     conn.sendFile(m.chat, img_url, 'eror.jpg', pesan, m, 0, { thumbnail: await (await fetch(img_url)).buffer() })
-    conn.sendFile(m.chat, mp3_url, 'error.mp3', '', m, 0, { asDocument: global.db.data.chats[m.chat].useDocument, mimetype: 'audio/mp4' })
-
+    conn.sendFile(m.chat, mp3_url, 'error.mp3', '', m, 0, { asDocument: db.data.chats[m.chat].useDocument, mimetype: 'audio/mp4' })
 }
 handler.help = ['joox'].map(v => v + ' <judul>')
 handler.tags = ['downloader']
