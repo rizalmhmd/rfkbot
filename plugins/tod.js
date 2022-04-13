590e5fd0db6040f3f5e8dedb947e97433803979e
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-04-13 12:44:30 +0700

    Update tod.js

--- plugins/tod.js
+++ plugins/tod.js
@@ -1,27 +1,33 @@
 let fetch = require('node-fetch')
-
-let handler = async (m, { conn, command, usedPrefix }) => {
+let handler = async (m, { conn, command }) => {
   if (/^tod$/i.test(command)) {
-    await conn.send3Button(m.chat, 'Truth or Dare', 'ROZZxBOTZ', 'Truth', `${usedPrefix}truth`, 'Dare', `${usedPrefix}dare`, 'Acak', `${conn.pickRandom([`${usedPrefix}dare`, `${usedPrefix}truth`])}`, m)
+    conn.send3Button(m.chat, 'Truth or Dare', footer, 'Truth', '#truth', 'Dare', '#dare', 'RANDOM', `${pickRandom(['#dare', '#truth'])}`, m, { contextInfo: { forwardingScore: 999, isForwarded: true }})
   }
   if (/^truth$/i.test(command)) {
-    let res = await fetch(API('amel', '/truth', {}, 'apikey'))
+    let res = await fetch(global.API('amel', '/truth', {}, 'apikey'))
     if (!res.ok) throw eror
     let json = await res.json()
-    if (!json.status) throw json
-    conn.send2Button(m.chat, json.result, 'ROZZxBOTZ', 'Truth', `${usedPrefix}truth`, 'Dare', `${usedPrefix}dare`, m)
+    if (json.message == "") throw json
+    conn.send2Button(m.chat, json.message, footer, 'Truth', ',#truth', 'Dare', '#dare', m, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+
+
   }
   if (/^dare$/i.test(command)) {
-    let res = await fetch(API('amel', '/dare', {}, 'apikey'))
+    let res = await fetch(global.API('amel', '/dare', {}, 'apikey'))
     if (!res.ok) throw eror
     let json = await res.json()
-    if (!json.status) throw json
-    conn.send2Button(m.chat, json.result, 'ROZZxBOTZ', 'Truth', `${usedPrefix}truth`, 'Dare', `${usedPrefix}dare`, m)
+    if (json.message == "") throw json
+    conn.send2Button(m.chat, json.message, footer, 'Truth', '#truth', 'Dare', '#dare', m, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+
+
   }
 }
 handler.help = ['tod']
 handler.tags = ['fun']
 handler.command = /^(tod|truth|dare)$/i
 
-module.exports = handler 
+module.exports = handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
 }
