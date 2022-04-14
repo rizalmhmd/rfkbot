7843b189250b3ef9addf63914545d92ddf4acf77
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-04-14 22:47:06 +0700

    Update blowjob.js

--- plugins/blowjob.js
+++ plugins/blowjob.js
@@ -1,9 +1,9 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix, command }) => {
 	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
-   heum = await fetch(`https://api.lolhuman.xyz/api/random2/bj?apikey=Deffbotz`)
+   heum = await fetch(`https://api.xteam.xyz/randomimage/blowjob?apikey=9db5a5373433c015`)
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '🔥🥵🥵', '©mikebot', 'NEXT', `${usedPrefix + command}`, m)
+   conn.sendButtonImg(m.chat, json, '🔥🥵🥵', 'ROZZxBOTZ', 'NEXT', `${usedPrefix + command}`, m)
 }
 handler.help = ['blowjob']
 handler.tags = ['hentai']
