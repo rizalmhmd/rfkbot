9ba41612371ef930e76ed48a131263ac707153cf
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-04-14 18:48:46 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,25 +1,23 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
-if (!args[0]) throw 'Uhm..url nya mana?'
-m.reply(wait)
-let res = await fetch(`https://api.lolhuman.xyz/api/tiktok?apikey=${lolkey}&url=${args[0]}`)
-if (!res.ok) throw await res.text()
-let json = await res.json()
-if (!json.status) throw json
-let { description, author, statistic, link } = json.result
-await conn.sendFile(m.chat, link, 'tt.mp4', `
-▶ ${statistic.playCount} Views
-❤ ${statistic.diggCount} Likes
-🔁 ${statistic.shareCount} Shares
-💬 ${statistic.commentCount} Comments
-- *By:* ${author.nickname} (${author.username})
-- *Desc:*
-${description}
-`.trim(), m)
+  if (!args[0]) throw 'Uhm...url nya mana?'
+  let res = await fetch(global.API('xteam', '/dl/tiktok', {
+    url: args[0]
+  }, 'APIKEY'))
+  if (res.status !== 200) throw await res.text()
+  let json = await res.json()
+  if (!json.status) throw json
+  try {
+    await conn.sendFile(m.chat, json.server_1, 'tiktok.mp4', '', m)
+  } catch (e) {
+    m.reply('Server 1 Failed, Retrying with Server 2')
+    await conn.sendFile(m.chat, json.server_2, 'tiktok.mp4', '', m)
+  }
 }
-
-handler.help = ['tiktok <url>']
+handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^tiktok$/i
+
+handler.premium = false
+handler.command = /^(tiktok(dl)?)$/i
 
 module.exports = handler
