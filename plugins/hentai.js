6b34f95da8c91c070465d612b7155514edba309f
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-04-14 22:42:37 +0700

    Update hentai.js

--- plugins/hentai.js
+++ plugins/hentai.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix, command }) => {
 if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
 m.reply(wait)
-heum = await fetch(`https://api.shizukaa.xyz/api/randomimage?apikey=itsmeiky633`)
+heum = await fetch(`https://api.xteam.xyz/randomimage/hentai?APIKEY=9db5a5373433c015`)
    if (!heum) throw json
     json = await heum.buffer()
    conn.sendButtonImg(m.chat, json, kasihcaption, footer, 'NEXT', `${usedPrefix + command}`, m)
