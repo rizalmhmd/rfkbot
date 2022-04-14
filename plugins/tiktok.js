23f1e62a3ca2b8c894b3733911f9e639e7e488fa
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2022-04-14 21:37:32 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,23 +1,19 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, args }) => {
-  if (!args[0]) throw 'Uhm...url nya mana?'
-  let res = await fetch(global.API('xteam', '/dl/tiktok', {
-    url: args[0]
-  }, 'APIKEY'))
-  if (res.status !== 200) throw await res.text()
-  let json = await res.json()
-  if (!json.status) throw json
-  try {
-    await conn.sendFile(m.chat, json.server_1, 'tiktok.mp4', '', m)
-  } catch (e) {
-    m.reply('Server 1 Failed, Retrying with Server 2')
-    await conn.sendFile(m.chat, json.server_2, 'tiktok.mp4', '', m)
-  }
+const { tiktokmusic } = require("../../Lib/scrape.js")
+module.exports = {
+name: ["tiktok", "tt"],
+type: ["downloader"],
+description: "download video from tiktok with url",
+utilisation: userbot.prefix + "tiktok <url>",
+
+async execute(m) {
+let { conn, args } = data
+try {
+if (!args[0]) return m.reply('mana urlnya')
+let ttdata = await tiktokmusic(args[0])
+let teks = `*Nama :* ${ttdata.meta.author.nickname}\n*Deskripsi :* ${ttdata.meta.desc}\n*Durasi* : ${ttdata.meta.video.duration}\n\n_Pilih Tipe Dibawah Ini_  ${m.mention}`
+conn.send2ButtonImg(m.chat, teks, await Ft.getBuffer(ttdata.meta.video.cover), userbot.packname, "MP3 🎧","tmusic "+args[0], "MP4 🎥", "tnowm "+args[0], {contextInfo:{"mentionedJid": conn.parseMention(teks)}})
+} catch (e) {
+m.reply(Ft.util.format(e))
 }
-handler.help = ['tiktok'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-
-handler.premium = false
-handler.command = /^(tiktok(dl)?)$/i
-
-module.exports = handler
+}
+}
